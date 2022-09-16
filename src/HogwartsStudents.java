1fc87a1d3cd4e1e671f0ddf537af2f64c3e13c69
Voldem999 <vs-kat@mail.ru>
2022-09-16 10:03:24 +0700

    домашняя работа

---
+++ src/HogwartsStudents.java
@@ -0,0 +1,44 @@
+public abstract class HogwartsStudents {
+
+    private String name;
+    private int magic;
+    private int transgression;
+
+    public HogwartsStudents(String name, int magic, int transgression) {
+        this.name = name;
+        this.magic = magic;
+        this.transgression = transgression;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+    public void setTransgression(int transgression) {
+        this.transgression = transgression;
+    }
+
+    public int ability() {
+        return magic + transgression;
+    }
+
+    public void compareHogwarts(HogwartsStudents hogwartsStudents) {
+
+    }
+}
